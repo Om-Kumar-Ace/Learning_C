2181861b889946e393d3f71a18d08ecb88e02f1d
Om-Kumar-Ace <om2005kumar001@gmail.com>
2023-11-14 13:23:35 +0530

    update

--- variabels.c
+++ variabels.c
@@ -5,7 +5,10 @@ int main()
     int a = 4;
     float b = 8.5;
     char c = 'u';
-    printf("The value of a is %d ", a);
-    printf("The value of a is %f ", b);
+    int d = 45 ;
+    printf("The value of a is %d \n", a);
+    printf("The value of b is %f \n", b);
+    printf("The value of c is %c \n", c);
+    printf("The sum of a and d is %d \n", a+d);
     return 0;
 }
