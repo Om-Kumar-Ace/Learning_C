7aa80515f5cab34e6b6ea9d6d310ea84b5257b90
Om-Kumar-Ace <om2005kumar001@gmail.com>
2023-11-14 12:26:54 +0530

    variabels

---
+++ variabels.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = 4;
+    float b = 8.5;
+    char c = 'u';
+    printf("The value of a is %d ", a);
+    printf("The value of a is %f ", b);
+    return 0;
+}
