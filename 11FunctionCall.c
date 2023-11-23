24a684cfd238320c8604c4f9e76d4a495ad64eae
Om-Kumar-Ace <om2005kumar001@gmail.com>
2023-11-23 20:49:43 +0530

    New Chapter

---
+++ 11FunctionCall.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void Gm();
+void Ga();
+void Gn();
+
+int main(){
+    Gm();
+    Ga();
+    Gn();
+    return 0;
+}
+void Gm(){
+    printf("Good Morning \n");
+}
+void Ga(){
+    printf("Good Afternoon \n");
+}
+void Gn(){
+    printf("Good Night \n");
+}
